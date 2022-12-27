e3b601281577a5f663d4252e483d2d283b0ca0ac
sgu6ena <sgu6ena@gmail.com>
2022-12-27 11:16:16 +0200

    styles

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.css"
 const Layout: FC<{children:React.ReactNode}> = ({children}) => {
   return (
     <div className={styles.layout}>
-      <h1>Telegram Bot Admin</h1>
+      <h1>Telegram Bot Admin Panel</h1>
       {children}
       <div className={styles.bottom}>
         {/*<img src="img/bg.jpg" alt=""/>*/}
--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,6 +1,6 @@
 import {FC } from 'react';
 
-import {  Tabs } from 'antd';
+import {Segmented, Tabs} from 'antd';
 import Bender from "../../components/bender/bender";
 const items = [
   { label: 'Добавление нового пользователя', key: 'item-1', children: 'Content 1', }, // remember to pass the key prop
@@ -11,15 +11,18 @@ const Main:FC = () => {
 
   return (
     <div>
-      <Tabs
-        tabBarStyle={{color: 'white', fontSize:'45px'}}
-        centered
-        tabBarGutter={20}
-        defaultActiveKey="1"
-        type="card"
-        size='large'
-        items={items}
-      />
+      {/*<Tabs*/}
+      {/*  tabBarStyle={{color: 'white', fontSize:'45px'}}*/}
+      {/*  centered*/}
+      {/*  */}
+      {/*  tabBarGutter={10}*/}
+      {/*  defaultActiveKey="1"*/}
+      {/*  type="card"*/}
+      {/*  size='large'*/}
+      {/*  items={items}*/}
+      {/*/>*/}
+      <div style={{display:'flex', justifyContent:'center'}}><Segmented  size="large" options={['Новый пользователь', 'Все пользователи']} /></div>
+      
       <Bender/>
     </div>
   );
